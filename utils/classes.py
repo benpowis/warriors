ff0fe8e58b9f765a7ccd99ca407709da1bb83fe2
Ben <benillus@gmail.com>
2025-01-10 13:10:27 +0000

    Added onster weights so toucher monsters are less common to encounter

--- utils/classes.py
+++ utils/classes.py
@@ -42,83 +42,98 @@ class EquipmentSlots:
 class Enemy:
     def __init__(self, area):
         enemies = {
-    "forest_easy": [
-        {"name": "Wolf", "health": 50, "strength": 8, "armour": 3, "xp": 30, "gold": 15, "image": "wolf.png"},
-        {"name": "Bandit", "health": 60, "strength": 10, "armour": 5, "xp": 40, "gold": 25, "image": "bandit.png"},
-        {"name": "Wild Boar", "health": 55, "strength": 9, "armour": 6, "xp": 35, "gold": 20, "image": "boar.png"},
-        {"name": "Goblin Scout", "health": 45, "strength": 7, "armour": 2, "xp": 25, "gold": 30, "image": "goblin.png"},
-        {"name": "Giant Spider", "health": 40, "strength": 12, "armour": 4, "xp": 35, "gold": 18, "image": "spider.png"},
-        {"name": "Forest Imp", "health": 35, "strength": 6, "armour": 1, "xp": 20, "gold": 35, "image": "imp.png"},
-        {"name": "Rabid Bear", "health": 65, "strength": 11, "armour": 8, "xp": 45, "gold": 22, "image": "bear.png"},
-        {"name": "Hostile Hunter", "health": 55, "strength": 9, "armour": 4, "xp": 32, "gold": 28, "image": "hunter.png"}
-    ],
-    "forest_medium": [
-        {"name": "Troll", "health": 80, "strength": 12, "armour": 12, "xp": 50, "gold": 40, "image": "troll.png"},
-        {"name": "Dark Dwarf", "health": 70, "strength": 15, "armour": 15, "xp": 45, "gold": 50, "image": "dark_dwarf.png"},
-        {"name": "Forest Ogre", "health": 90, "strength": 14, "armour": 10, "xp": 55, "gold": 45, "image": "ogre.png"},
-        {"name": "Werewolf", "health": 75, "strength": 16, "armour": 8, "xp": 60, "gold": 55, "image": "werewolf.png"},
-        {"name": "Corrupted Ent", "health": 100, "strength": 13, "armour": 20, "xp": 58, "gold": 48, "image": "ent.png"},
-        {"name": "Harpy Warrior", "health": 65, "strength": 18, "armour": 6, "xp": 52, "gold": 60, "image": "harpy.png"},
-        {"name": "Dire Wolf Pack", "health": 85, "strength": 15, "armour": 7, "xp": 57, "gold": 52, "image": "dire_wolf.png"},
-        {"name": "Forest Witch", "health": 60, "strength": 20, "armour": 5, "xp": 65, "gold": 70, "image": "witch.png"},
-        {"name": "Shambling Mound", "health": 95, "strength": 14, "armour": 18, "xp": 54, "gold": 45, "image": "mound.png"},
-        {"name": "Bandit Chief", "health": 70, "strength": 17, "armour": 14, "xp": 56, "gold": 65, "image": "bandit_chief.png"}
-    ],
-    "forest_hard": [
-        {"name": "Dragon", "health": 100, "strength": 20, "armour": 25, "xp": 80, "gold": 100, "image": "dragon.png"},
-        {"name": "Giant", "health": 120, "strength": 18, "armour": 20, "xp": 70, "gold": 80, "image": "giant.png"},
-        {"name": "Ancient Treant", "health": 150, "strength": 16, "armour": 30, "xp": 85, "gold": 90, "image": "treant.png"},
-        {"name": "Demon Hunter", "health": 90, "strength": 25, "armour": 18, "xp": 90, "gold": 120, "image": "demon_hunter.png"},
-        {"name": "Forest Hydra", "health": 130, "strength": 22, "armour": 22, "xp": 95, "gold": 110, "image": "hydra.png"},
-        {"name": "Dark Elf Champion", "health": 85, "strength": 28, "armour": 16, "xp": 100, "gold": 150, "image": "dark_elf.png"},
-        {"name": "Corrupted Unicorn", "health": 110, "strength": 24, "armour": 15, "xp": 88, "gold": 130, "image": "unicorn.png"},
-        {"name": "Elder Wyrm", "health": 140, "strength": 21, "armour": 28, "xp": 92, "gold": 140, "image": "wyrm.png"},
-        {"name": "Shadow Giant", "health": 160, "strength": 19, "armour": 24, "xp": 87, "gold": 95, "image": "shadow_giant.png"}
-    ],
-    "mountain_easy": [
-        {"name": "Mountain Goat", "health": 80, "strength": 12, "armour": 5, "xp": 50, "gold": 30, "image": "placeholder.png"},
-        {"name": "Rock Elemental", "health": 90, "strength": 15, "armour": 15, "xp": 60, "gold": 40, "image": "placeholder.png"},
-        {"name": "Snow Wolf", "health": 75, "strength": 14, "armour": 4, "xp": 45, "gold": 35, "image": "placeholder.png"},
-        {"name": "Frost Imp", "health": 65, "strength": 10, "armour": 2, "xp": 40, "gold": 45, "image": "placeholder.png"},
-        {"name": "Crystal Spider", "health": 70, "strength": 13, "armour": 8, "xp": 55, "gold": 38, "image": "placeholder.png"},
-        {"name": "Ice Goblin", "health": 85, "strength": 11, "armour": 6, "xp": 48, "gold": 42, "image": "placeholder.png"},
-        {"name": "Cave Dweller", "health": 95, "strength": 13, "armour": 10, "xp": 52, "gold": 36, "image": "placeholder.png"},
-        {"name": "Mountain Bandit", "health": 88, "strength": 14, "armour": 7, "xp": 54, "gold": 48, "image": "placeholder.png"}
-    ],
-    "mountain_medium": [
-        {"name": "Frost Giant", "health": 120, "strength": 18, "armour": 18, "xp": 80, "gold": 60, "image": "placeholder.png"},
-        {"name": "Ice Troll", "health": 100, "strength": 20, "armour": 15, "xp": 75, "gold": 70, "image": "placeholder.png"},
-        {"name": "Yeti", "health": 130, "strength": 22, "armour": 12, "xp": 85, "gold": 65, "image": "placeholder.png"},
-        {"name": "Storm Harpy", "health": 95, "strength": 24, "armour": 8, "xp": 78, "gold": 75, "image": "placeholder.png"},
-        {"name": "Frost Wyrm", "health": 140, "strength": 19, "armour": 20, "xp": 82, "gold": 68, "image": "placeholder.png"},
-        {"name": "Ice Witch", "health": 90, "strength": 26, "armour": 10, "xp": 88, "gold": 85, "image": "placeholder.png"},
-        {"name": "Mountain Ogre", "health": 150, "strength": 21, "armour": 16, "xp": 76, "gold": 72, "image": "placeholder.png"},
-        {"name": "Avalanche Spirit", "health": 110, "strength": 23, "armour": 14, "xp": 84, "gold": 78, "image": "placeholder.png"},
-        {"name": "Crystal Golem", "health": 160, "strength": 20, "armour": 25, "xp": 86, "gold": 66, "image": "placeholder.png"},
-        {"name": "Ice Drake", "health": 125, "strength": 25, "armour": 22, "xp": 90, "gold": 80, "image": "placeholder.png"}
-    ],
-    "mountain_hard": [
-        {"name": "Ancient Dragon", "health": 200, "strength": 25, "armour": 35, "xp": 120, "gold": 150, "image": "placeholder.png"},
-        {"name": "Mountain Titan", "health": 180, "strength": 28, "armour": 30, "xp": 130, "gold": 160, "image": "placeholder.png"},
-        {"name": "Glacier Queen", "health": 170, "strength": 30, "armour": 25, "xp": 140, "gold": 180, "image": "placeholder.png"},
-        {"name": "Storm Giant King", "health": 220, "strength": 26, "armour": 32, "xp": 125, "gold": 165, "image": "placeholder.png"},
-        {"name": "Elder Frost Wyrm", "health": 190, "strength": 29, "armour": 38, "xp": 135, "gold": 175, "image": "placeholder.png"},
-        {"name": "Mountain Overlord", "health": 210, "strength": 27, "armour": 34, "xp": 145, "gold": 190, "image": "placeholder.png"},
-        {"name": "Crystal Behemoth", "health": 230, "strength": 24, "armour": 40, "xp": 128, "gold": 155, "image": "placeholder.png"},
-        {"name": "Eternal Ice Elemental", "health": 185, "strength": 31, "armour": 28, "xp": 138, "gold": 170, "image": "placeholder.png"},
-        {"name": "Blizzard Demon", "health": 195, "strength": 32, "armour": 26, "xp": 150, "gold": 200, "image": "placeholder.png"},
-        {"name": "Ancient Frost Giant", "health": 240, "strength": 28, "armour": 36, "xp": 142, "gold": 185, "image": "placeholder.png"},
-        {"name": "Mountain Dragon Lord", "health": 250, "strength": 35, "armour": 45, "xp": 160, "gold": 250, "image": "placeholder.png"}
-    ]
-}
-        enemy_type = random.choice(enemies[area])
-        self.name = enemy_type["name"]
-        self.health = enemy_type["health"]
-        self.strength = enemy_type["strength"]
-        self.armour = enemy_type["armour"]
-        self.xp = enemy_type["xp"]
-        self.gold = enemy_type["gold"]
-        self.image = enemy_type["image"]
+        "forest_easy": [
+            {"name": "Forest Imp", "health": 35, "strength": 6, "armour": 1, "xp": 20, "gold": 35, "image": "imp.png", "weight": 25},  # Weakest, most common
+            {"name": "Goblin Scout", "health": 45, "strength": 7, "armour": 2, "xp": 25, "gold": 30, "image": "goblin.png", "weight": 20},
+            {"name": "Wolf", "health": 50, "strength": 8, "armour": 3, "xp": 30, "gold": 15, "image": "wolf.png", "weight": 15},
+            {"name": "Giant Spider", "health": 40, "strength": 12, "armour": 4, "xp": 35, "gold": 18, "image": "spider.png", "weight": 15},
+            {"name": "Hostile Hunter", "health": 55, "strength": 9, "armour": 4, "xp": 32, "gold": 28, "image": "hunter.png", "weight": 10},
+            {"name": "Bandit", "health": 60, "strength": 10, "armour": 5, "xp": 40, "gold": 25, "image": "bandit.png", "weight": 8},
+            {"name": "Wild Boar", "health": 55, "strength": 9, "armour": 6, "xp": 35, "gold": 20, "image": "boar.png", "weight": 5},
+            {"name": "Rabid Bear", "health": 65, "strength": 11, "armour": 8, "xp": 45, "gold": 22, "image": "bear.png", "weight": 2}  # Strongest, rarest
+        ],
+        "forest_medium": [
+            {"name": "Harpy Warrior", "health": 65, "strength": 18, "armour": 6, "xp": 52, "gold": 60, "image": "harpy.png", "weight": 20},
+            {"name": "Forest Witch", "health": 60, "strength": 20, "armour": 5, "xp": 65, "gold": 70, "image": "witch.png", "weight": 15},
+            {"name": "Werewolf", "health": 75, "strength": 16, "armour": 8, "xp": 60, "gold": 55, "image": "werewolf.png", "weight": 15},
+            {"name": "Dark Dwarf", "health": 70, "strength": 15, "armour": 15, "xp": 45, "gold": 50, "image": "dark_dwarf.png", "weight": 12},
+            {"name": "Bandit Chief", "health": 70, "strength": 17, "armour": 14, "xp": 56, "gold": 65, "image": "bandit_chief.png", "weight": 10},
+            {"name": "Dire Wolf Pack", "health": 85, "strength": 15, "armour": 7, "xp": 57, "gold": 52, "image": "dire_wolf.png", "weight": 10},
+            {"name": "Troll", "health": 80, "strength": 12, "armour": 12, "xp": 50, "gold": 40, "image": "troll.png", "weight": 8},
+            {"name": "Forest Ogre", "health": 90, "strength": 14, "armour": 10, "xp": 55, "gold": 45, "image": "ogre.png", "weight": 5},
+            {"name": "Shambling Mound", "health": 95, "strength": 14, "armour": 18, "xp": 54, "gold": 45, "image": "mound.png", "weight": 3},
+            {"name": "Corrupted Ent", "health": 100, "strength": 13, "armour": 20, "xp": 58, "gold": 48, "image": "ent.png", "weight": 2}
+        ],
+        "forest_hard": [
+            {"name": "Dark Elf Champion", "health": 85, "strength": 28, "armour": 16, "xp": 100, "gold": 150, "image": "dark_elf.png", "weight": 25},
+            {"name": "Demon Hunter", "health": 90, "strength": 25, "armour": 18, "xp": 90, "gold": 120, "image": "demon_hunter.png", "weight": 20},
+            {"name": "Corrupted Unicorn", "health": 110, "strength": 24, "armour": 15, "xp": 88, "gold": 130, "image": "unicorn.png", "weight": 15},
+            {"name": "Giant", "health": 120, "strength": 18, "armour": 20, "xp": 70, "gold": 80, "image": "giant.png", "weight": 12},
+            {"name": "Forest Hydra", "health": 130, "strength": 22, "armour": 22, "xp": 95, "gold": 110, "image": "hydra.png", "weight": 10},
+            {"name": "Elder Wyrm", "health": 140, "strength": 21, "armour": 28, "xp": 92, "gold": 140, "image": "wyrm.png", "weight": 8},
+            {"name": "Shadow Giant", "health": 160, "strength": 19, "armour": 24, "xp": 87, "gold": 95, "image": "shadow_giant.png", "weight": 5},
+            {"name": "Ancient Treant", "health": 150, "strength": 16, "armour": 30, "xp": 85, "gold": 90, "image": "treant.png", "weight": 3},
+            {"name": "Dragon", "health": 100, "strength": 20, "armour": 25, "xp": 80, "gold": 100, "image": "dragon.png", "weight": 2}
+        ],
+        "mountain_easy": [
+            {"name": "Frost Imp", "health": 65, "strength": 10, "armour": 2, "xp": 40, "gold": 45, "image": "placeholder.png", "weight": 25},
+            {"name": "Snow Wolf", "health": 75, "strength": 14, "armour": 4, "xp": 45, "gold": 35, "image": "placeholder.png", "weight": 20},
+            {"name": "Ice Goblin", "health": 85, "strength": 11, "armour": 6, "xp": 48, "gold": 42, "image": "placeholder.png", "weight": 15},
+            {"name": "Mountain Bandit", "health": 88, "strength": 14, "armour": 7, "xp": 54, "gold": 48, "image": "placeholder.png", "weight": 15},
+            {"name": "Crystal Spider", "health": 70, "strength": 13, "armour": 8, "xp": 55, "gold": 38, "image": "placeholder.png", "weight": 10},
+            {"name": "Mountain Goat", "health": 80, "strength": 12, "armour": 5, "xp": 50, "gold": 30, "image": "placeholder.png", "weight": 8},
+            {"name": "Cave Dweller", "health": 95, "strength": 13, "armour": 10, "xp": 52, "gold": 36, "image": "placeholder.png", "weight": 5},
+            {"name": "Rock Elemental", "health": 90, "strength": 15, "armour": 15, "xp": 60, "gold": 40, "image": "placeholder.png", "weight": 2}
+        ],
+        "mountain_medium": [
+            {"name": "Storm Harpy", "health": 95, "strength": 24, "armour": 8, "xp": 78, "gold": 75, "image": "placeholder.png", "weight": 20},
+            {"name": "Ice Witch", "health": 90, "strength": 26, "armour": 10, "xp": 88, "gold": 85, "image": "placeholder.png", "weight": 15},
+            {"name": "Ice Troll", "health": 100, "strength": 20, "armour": 15, "xp": 75, "gold": 70, "image": "placeholder.png", "weight": 15},
+            {"name": "Avalanche Spirit", "health": 110, "strength": 23, "armour": 14, "xp": 84, "gold": 78, "image": "placeholder.png", "weight": 12},
+            {"name": "Frost Giant", "health": 120, "strength": 18, "armour": 18, "xp": 80, "gold": 60, "image": "placeholder.png", "weight": 10},
+            {"name": "Mountain Ogre", "health": 150, "strength": 21, "armour": 16, "xp": 76, "gold": 72, "image": "placeholder.png", "weight": 10},
+            {"name": "Yeti", "health": 130, "strength": 22, "armour": 12, "xp": 85, "gold": 65, "image": "placeholder.png", "weight": 8},
+            {"name": "Frost Wyrm", "health": 140, "strength": 19, "armour": 20, "xp": 82, "gold": 68, "image": "placeholder.png", "weight": 5},
+            {"name": "Ice Drake", "health": 125, "strength": 25, "armour": 22, "xp": 90, "gold": 80, "image": "placeholder.png", "weight": 3},
+            {"name": "Crystal Golem", "health": 160, "strength": 20, "armour": 25, "xp": 86, "gold": 66, "image": "placeholder.png", "weight": 2}
+        ],
+        "mountain_hard": [
+            {"name": "Glacier Queen", "health": 170, "strength": 30, "armour": 25, "xp": 140, "gold": 180, "image": "placeholder.png", "weight": 20},
+            {"name": "Eternal Ice Elemental", "health": 185, "strength": 31, "armour": 28, "xp": 138, "gold": 170, "image": "placeholder.png", "weight": 15},
+            {"name": "Blizzard Demon", "health": 195, "strength": 32, "armour": 26, "xp": 150, "gold": 200, "image": "placeholder.png", "weight": 15},
+            {"name": "Elder Frost Wyrm", "health": 190, "strength": 29, "armour": 38, "xp": 135, "gold": 175, "image": "placeholder.png", "weight": 12},
+            {"name": "Mountain Titan", "health": 180, "strength": 28, "armour": 30, "xp": 130, "gold": 160, "image": "placeholder.png", "weight": 10},
+            {"name": "Storm Giant King", "health": 220, "strength": 26, "armour": 32, "xp": 125, "gold": 165, "image": "placeholder.png", "weight": 8},
+            {"name": "Mountain Overlord", "health": 210, "strength": 27, "armour": 34, "xp": 145, "gold": 190, "image": "placeholder.png", "weight": 8},
+            {"name": "Ancient Frost Giant", "health": 240, "strength": 28, "armour": 36, "xp": 142, "gold": 185, "image": "placeholder.png", "weight": 5},
+            {"name": "Crystal Behemoth", "health": 230, "strength": 24, "armour": 40, "xp": 128, "gold": 155, "image": "placeholder.png", "weight": 4},
+            {"name": "Ancient Dragon", "health": 200, "strength": 25, "armour": 35, "xp": 120, "gold": 150, "image": "placeholder.png", "weight": 2},
+            {"name": "Mountain Dragon Lord", "health": 250, "strength": 35, "armour": 45, "xp": 160, "gold": 250, "image": "placeholder.png", "weight": 1}  # Ultimate boss, very rare
+        ]
+        }
+        area_enemies = enemies[area]
+        # Calculate total weight
+        total_weight = sum(enemy["weight"] for enemy in area_enemies)
+        
+        # Random roll between 1 and total weight
+        roll = random.randint(1, total_weight)
+        
+        # Select enemy based on weight
+        current_weight = 0
+        selected_enemy = None
+        for enemy in area_enemies:
+            current_weight += enemy["weight"]
+            if roll <= current_weight:
+                selected_enemy = enemy
+                break
+    
+        # Set enemy attributes
+        self.name = selected_enemy["name"]
+        self.health = selected_enemy["health"]
+        self.strength = selected_enemy["strength"]
+        self.xp = selected_enemy["xp"]
+        self.gold = selected_enemy["gold"]
+        self.image = selected_enemy["image"]
 
 class Buff:
     def __init__(self, name, stat, value, duration, icon):
